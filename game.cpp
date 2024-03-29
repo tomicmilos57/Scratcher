970cf554428fe71a114a2a67fed2faadcfa8325b
Milos Tomic laptop <tomicmilos57@gmail.com>
2024-03-29 21:41:53 +0100

    Undo option implemented

--- game.cpp
+++ game.cpp
@@ -2,169 +2,197 @@
 #include <SFML/Graphics/CircleShape.hpp>
 #include <SFML/Graphics/Color.hpp>
 #include <SFML/Window/Event.hpp>
+#include <SFML/Window/Keyboard.hpp>
 #include <SFML/Window/Mouse.hpp>
 #include <fstream>
 #include <iostream>
 #include <math.h>
 sf::Color Game::colors[] = {sf::Color::White,   sf::Color::Blue,
-                            sf::Color::Cyan,    sf::Color::Green,
-                            sf::Color::Magenta, sf::Color::Red,
-                            sf::Color::Yellow}; //, sf::Color::Black
+    sf::Color::Cyan,    sf::Color::Green,
+    sf::Color::Magenta, sf::Color::Red,
+    sf::Color::Yellow}; //, sf::Color::Black
 bool newstroke = true;
 void Game::event() {
-  sf::Event event;
+    sf::Event event;
 
-  static Point prev(0, 0, 0, 0);
-  while (window.pollEvent(event)) {
-    if (event.type == sf::Event::Closed) {
-      // this->saveGame();
-      window.close();
+    static Stroke *stroke = new Stroke();
+    static Point prev(0, 0, 0, 0);
+    while (window.pollEvent(event)) {
+        if (event.type == sf::Event::Closed) {
+            // this->saveGame();
+            window.close();
+        }
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+            sf::Vector2i position = sf::Mouse::getPosition(window);
+            Point p(position.x, position.y, color);
+            if (newstroke) { // this could be improved
+                clear_stack();
+                prev = p;
+                newstroke = false;
+                stroke = new Stroke();
+                arr.push_back(stroke);
+            }
+            //insert_pixel(p);
+            if(stroke != arr.back()) exit(1);
+            fill_line(stroke, p.x, p.y, prev.x, prev.y);
+            prev = p;
+        }
+        if (event.type == sf::Event::MouseButtonReleased) {
+            newstroke = true;
+            event.type = sf::Event::Count; // Needed so ButtonReleased works correctly
+        }
+        if (event.type == sf::Event::KeyPressed) {
+            if (event.key.scancode == sf::Keyboard::Scan::PageUp)
+                incRadius();
+            if (event.key.scancode == sf::Keyboard::Scan::PageDown)
+                decRadius();
+            if (event.key.scancode == sf::Keyboard::Scan::Up)
+                incColor();
+            if (event.key.scancode == sf::Keyboard::Scan::Down)
+                decColor();
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::LControl)
+                    && sf::Keyboard::isKeyPressed(sf::Keyboard::Z)
+                    &&  sf::Keyboard::isKeyPressed(sf::Keyboard::LShift)){
+                if(!stack_undo.empty()){ 
+                    arr.push_back(stack_undo.top());//App crashes if arr is empty
+                    stack_undo.pop();
+                }
+            }
+            else if (sf::Keyboard::isKeyPressed(sf::Keyboard::LControl)
+                    && sf::Keyboard::isKeyPressed(sf::Keyboard::Z)){
+                if(!arr.empty()){
+                    stack_undo.push(arr.back());//App crashes if arr is empty
+                    arr.pop_back();
+                }
+            }
+            /*if (event.key.scancode == sf::Keyboard::Scan::Delete)
+              color = 7; // Black SHOULD ACCTUALY REMOVE FROM SET*/
+        }
     }
-    if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
-      sf::Vector2i position = sf::Mouse::getPosition(window);
-      Point p(position.x, position.y, color);
-      if (newstroke) { // this could be improved
-        prev = p;
-        newstroke = false;
-      }
-      insert_pixel(p);
-      fill_line(p.x, p.y, prev.x, prev.y);
-      prev = p;
-    }
-    if (event.type == sf::Event::MouseButtonReleased) {
-      newstroke = true;
-      layer++;
-      event.type = sf::Event::Count; // Needed so ButtonReleased works correctly
-    }
-    if (event.type == sf::Event::KeyPressed) {
-      if (event.key.scancode == sf::Keyboard::Scan::PageUp)
-        incRadius();
-      if (event.key.scancode == sf::Keyboard::Scan::PageDown)
-        decRadius();
-      if (event.key.scancode == sf::Keyboard::Scan::Up)
-        incColor();
-      if (event.key.scancode == sf::Keyboard::Scan::Down)
-        decColor();
-      /*if (event.key.scancode == sf::Keyboard::Scan::Delete)
-        color = 7; // Black SHOULD ACCTUALY REMOVE FROM SET*/
-    }
-  }
 }
 
 void Game::draw() const {
 
-  // Drwaing points
-  for (auto &point : set) {
-    sf::Vertex dot(sf::Vector2f(point.x, point.y), colors[point.color]);
-    window.draw(&dot, 1, sf::Points);
-  }
-  // Drawing cursor
-  sf::CircleShape cursor;
-  cursor.setRadius(radius);
-  cursor.setOrigin(radius, radius);
-  cursor.setFillColor(colors[color]);
-  sf::Vector2i position = sf::Mouse::getPosition(window);
-  cursor.setPosition(position.x, position.y);
-  window.draw(cursor);
+    // Drwaing points
+    for (auto &stroke : arr){
+        for (auto &point : stroke->arr) {
+            sf::Vertex dot(sf::Vector2f(point.x, point.y), colors[point.color]);
+            window.draw(&dot, 1, sf::Points);
+        }
+    }
+    // Drawing cursor
+    sf::CircleShape cursor;
+    cursor.setRadius(radius);
+    cursor.setOrigin(radius, radius);
+    cursor.setFillColor(colors[color]);
+    sf::Vector2i position = sf::Mouse::getPosition(window);
+    cursor.setPosition(position.x, position.y);
+    window.draw(cursor);
 }
-void Game::saveGame() const {
+/*void Game::saveGame() const {
   std::cout << "Saving game" << std::endl;
   std::ofstream savefile("save.txt");
   for (auto &point : set) {
-    std::cout << point.x << ' ' << point.y << std::endl;
-    savefile << point;
+  std::cout << point.x << ' ' << point.y << std::endl;
+  savefile << point;
   }
   savefile.close();
   return;
-}
-void Game::loadGame() {
+  }
+  void Game::loadGame() {
   std::ifstream savefile("save.txt");
   while (!savefile.eof()) {     // getline(myfile,line)
-    Point *p = new Point(0, 0); // memleak
-    savefile >> *p;
-    std::cout << "Loading " << p->x << ' ' << p->y << std::endl;
-    set.insert(*p);
+  Point *p = new Point(0, 0); // memleak
+  savefile >> *p;
+  std::cout << "Loading " << p->x << ' ' << p->y << std::endl;
+  set.insert(*p);
   }
   savefile.close();
   return;
-} // insert_pixel(Point(x, y, color));
-void Game::fill_line(int x1, int y1, int x2, int y2) {
-  int x, y, dx, dy, dx1, dy1, px, py, xe, ye, i;
-  dx = x2 - x1;
-  dy = y2 - y1;
-  dx1 = fabs(dx);
-  dy1 = fabs(dy);
-  px = 2 * dy1 - dx1;
-  py = 2 * dx1 - dy1;
-  if (dy1 <= dx1) {
-    if (dx >= 0) {
-      x = x1;
-      y = y1;
-      xe = x2;
-    } else {
-      x = x2;
-      y = y2;
-      xe = x1;
-    }
-    insert_pixel(Point(x, y, color));
-    for (i = 0; x < xe; i++) {
-      x = x + 1;
-      if (px < 0) {
-        px = px + 2 * dy1;
-      } else {
-        if ((dx < 0 && dy < 0) || (dx > 0 && dy > 0)) {
-          y = y + 1;
+  } // insert_pixel(Point(x, y, color));*/
+void Game::fill_line(Stroke* stroke, int x1, int y1, int x2, int y2) {
+    int x, y, dx, dy, dx1, dy1, px, py, xe, ye, i;
+    dx = x2 - x1;
+    dy = y2 - y1;
+    dx1 = fabs(dx);
+    dy1 = fabs(dy);
+    px = 2 * dy1 - dx1;
+    py = 2 * dx1 - dy1;
+    if (dy1 <= dx1) {
+        if (dx >= 0) {
+            x = x1;
+            y = y1;
+            xe = x2;
         } else {
-          y = y - 1;
+            x = x2;
+            y = y2;
+            xe = x1;
         }
-        px = px + 2 * (dy1 - dx1);
-      }
-      insert_pixel(Point(x, y, color));
-    }
-  } else {
-    if (dy >= 0) {
-      x = x1;
-      y = y1;
-      ye = y2;
+        insert_pixel(stroke, Point(x, y, color));
+        for (i = 0; x < xe; i++) {
+            x = x + 1;
+            if (px < 0) {
+                px = px + 2 * dy1;
+            } else {
+                if ((dx < 0 && dy < 0) || (dx > 0 && dy > 0)) {
+                    y = y + 1;
+                } else {
+                    y = y - 1;
+                }
+                px = px + 2 * (dy1 - dx1);
+            }
+            insert_pixel(stroke, Point(x, y, color));
+        }
     } else {
-      x = x2;
-      y = y2;
-      ye = y1;
-    }
-    insert_pixel(Point(x, y, color));
-    for (i = 0; y < ye; i++) {
-      y = y + 1;
-      if (py <= 0) {
-        py = py + 2 * dx1;
-      } else {
-        if ((dx < 0 && dy < 0) || (dx > 0 && dy > 0)) {
-          x = x + 1;
+        if (dy >= 0) {
+            x = x1;
+            y = y1;
+            ye = y2;
         } else {
-          x = x - 1;
+            x = x2;
+            y = y2;
+            ye = y1;
         }
-        py = py + 2 * (dx1 - dy1);
-      }
-      insert_pixel(Point(x, y, color));
+        insert_pixel(stroke, Point(x, y, color));
+        for (i = 0; y < ye; i++) {
+            y = y + 1;
+            if (py <= 0) {
+                py = py + 2 * dx1;
+            } else {
+                if ((dx < 0 && dy < 0) || (dx > 0 && dy > 0)) {
+                    x = x + 1;
+                } else {
+                    x = x - 1;
+                }
+                py = py + 2 * (dx1 - dy1);
+            }
+            insert_pixel(stroke, Point(x, y, color));
+        }
     }
-  }
 }
 
-void Game::insert_pixel(Point p) {
-  if (p.x >= 0 && p.y >= 0 && p.x < width && p.y < height) {
-    set.insert(p);
-  }
+void Game::insert_pixel(Stroke *stroke, Point p) {
+    if (p.x >= 0 && p.y >= 0 && p.x < width && p.y < height) {
+        stroke->addPoint(p);
+    }
+}
+void Game::clear_stack(){
+    while(!stack_undo.empty()){
+        delete stack_undo.top();
+        stack_undo.pop();
+    }
 }
 
 /*sf::Vector2i position = sf::Mouse::getPosition(window);
-sf::CircleShape circle;
-circle.setPosition(position.x, position.y);
-circle.setRadius(radius);
-for (size_t i = 0; i < circle.getPointCount(); i++) {
+  sf::CircleShape circle;
+  circle.setPosition(position.x, position.y);
+  circle.setRadius(radius);
+  for (size_t i = 0; i < circle.getPointCount(); i++) {
   auto point = circle.getPoint(i);
   Point p(point.x + position.x - radius, point.y + position.y - radius,
-          color);
+  color);
   if (p.x >= 0 && p.y >= 0 && p.x < width && p.y < height) {
-    set.insert(p);
-    // std::cout << p.x << " " << p.y << std::endl;
-  }
+  set.insert(p);
+// std::cout << p.x << " " << p.y << std::endl;
+}
 }*/
--- game.h
+++ game.h
@@ -1,37 +1,39 @@
 #ifndef Game_Scratcher
 #define Game_Scratcher
-#include "point.h"
+#include "stroke.h"
 #include <SFML/Graphics.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/Window/Mouse.hpp>
 #include <iostream>
 #include <set>
+#include <stack>
 class Game {
-private:
-  sf::RenderWindow &window;
-  std::set<Point> set;
-  int width;
-  int height;
-  int radius = 1;
-  int color = 0;
-  int layer = 1;
-  static sf::Color colors[];
+    private:
+        sf::RenderWindow &window;
+        //std::set<Point> set;
+        std::vector<Stroke*>arr;
+        std::stack<Stroke*>stack_undo;
+        int width;
+        int height;
+        int radius = 1;
+        int color = 0;
+        static sf::Color colors[];
+    public:
+        Game(sf::RenderWindow &window, int w, int h)
+            : window(window), width(w), height(h) {}
+        void event();
+        void draw() const;
+    private:
+        void saveGame() const;
+        void loadGame();
+        void incRadius() { radius < 20 ? radius += 1 : 20; } 
+        void decRadius() { radius > 1 ? radius -= 1 : 1; }
+        void incColor() { color < 6 ? color += 1 : color = 0; }
+        void decColor() { color > 0 ? color -= 1 : color = 6; }
+        void insert_pixel(Stroke*,Point);
+        void fill_line(Stroke*, int, int, int, int);
+        void clear_stack();
 
-public:
-  Game(sf::RenderWindow &window, int w, int h)
-      : window(window), width(w), height(h) {}
-  void event();
-  void draw() const;
-  void saveGame() const;
-  void loadGame();
-  void incRadius() { radius < 20 ? radius += 1 : 20; }
-  void decRadius() { radius > 1 ? radius -= 1 : 1; }
-  void incColor() { color < 6 ? color += 1 : color = 0; }
-  void decColor() { color > 0 ? color -= 1 : color = 6; }
-
-private:
-  void insert_pixel(Point);
-  void fill_line(int, int, int, int);
 };
 
 #endif
